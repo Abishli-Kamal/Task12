ac697bcd2c3185829672841c65c8de9c7c197d44
Abishli-Kamal <tu7ldxfzy@code.edu.az>
2022-03-10 11:12:36 +0400

    task hazirdi

---
+++ task-1/task-1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine( Max());
+        }
+        public static int Max()
+        {
+            int[] arr = { 10, 8, 49, 17, 5 };
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+
+    }
+}
+
---
+++ task-2/task-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 4500;
+            int count = 0;
+            int result = 0;
+            int input = 3;
+            while (num > 0)
+            {
+
+                num = num / 10;
+                count++;
+                result = input * 10 * 10 * 10 * 10;
+
+            }
+            
+            Console.WriteLine(result + 4500);
+        }
+    }
+}
